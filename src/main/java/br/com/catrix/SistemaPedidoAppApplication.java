f72af6cb6b8288c1953c4811b876a7cb01045b49
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-23 21:30:36 -0300

    API do Sistema de Pedidos criada.

---
+++ src/main/java/br/com/catrix/SistemaPedidoAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.catrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPedidoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPedidoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/catrix/SistemaPedidoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPedidoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
