75a6b8f25c87f7a53acf8b133132edfdd6cc5818
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-24 18:19:28 -0300

    Inserindo comentário para entrar no padrão proposto no projeto.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- Inserindo dados da entidade Categoria --
 INSERT INTO CATEGORIA (ID, NOME) VALUES (1, 'informática');
 INSERT INTO CATEGORIA (ID, NOME) VALUES (2, 'Educação');
 INSERT INTO CATEGORIA (ID, NOME) VALUES (3, 'Eletrônico');
