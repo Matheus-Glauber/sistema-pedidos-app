c0e2f75ea1b1c7f50fd0b328625d1a015a1b522c
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-24 21:06:13 -0300

    ProdutoController criado.

--- src/main/java/br/com/catrix/controllers/ProdutoController.java
+++ src/main/java/br/com/catrix/controllers/ProdutoController.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.catrix.entities.Categoria;
-import br.com.catrix.services.CategoriaService;
+import br.com.catrix.entities.Produto;
+import br.com.catrix.services.ProdutoService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
-@RequestMapping(value = "/categorias")
-public class CategoriaController {
+@RequestMapping(value = "/produtos")
+public class ProdutoController {
 	
 	@Autowired
-	private CategoriaService categoriaService;
+	private ProdutoService produtoService;
 	
-	@ApiOperation(value = "Retorna uma lista de categorias")
+	@ApiOperation(value = "Retorna uma lista de produtos")
 	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Retorna a lista de categorias"),
+			@ApiResponse(code = 200, message = "Retorna a lista de produtos"),
 			@ApiResponse(code = 400, message = "Página não encontrada"),
 			@ApiResponse(code = 401, message = "Usuário não autenticado"),
 			@ApiResponse(code = 403, message = "Usuário sem permissão"),
@@ -32,14 +32,14 @@ public class CategoriaController {
 			@ApiResponse(code = 500, message = "Internal Server Error")			
 	})
 	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<List<Categoria>> listar() {	
-		List<Categoria> listaCategoria = categoriaService.buscarTodos();
-		return ResponseEntity.ok(listaCategoria);
+	public ResponseEntity<List<Produto>> listar() {	
+		List<Produto> listaProduto = produtoService.buscarTodosProdutos();
+		return ResponseEntity.ok(listaProduto);
 	}
 	
-	@ApiOperation(value = "Retorna uma categoria, definido pelo id")
+	@ApiOperation(value = "Retorna um produto, definido pelo id")
 	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Retorna a categoria"),
+			@ApiResponse(code = 200, message = "Retorna o produto"),
 			@ApiResponse(code = 400, message = "Página não encontrada"),
 			@ApiResponse(code = 401, message = "Usuário não autenticado"),
 			@ApiResponse(code = 403, message = "Usuário sem permissão"),
@@ -47,9 +47,9 @@ public class CategoriaController {
 			@ApiResponse(code = 500, message = "Internal Server Error")	
 	})
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}",produces = "application/json")
-	public ResponseEntity<Categoria> buscarPorId(@PathVariable Long id) {
-		Categoria categoriaRetorno = categoriaService.buscar(id);
-		return ResponseEntity.ok(categoriaRetorno);
+	public ResponseEntity<Produto> buscarPorId(@PathVariable Long id) {
+		Produto produtoRetorno = produtoService.buscarProdutoPorId(id);
+		return ResponseEntity.ok(produtoRetorno);
 	}
 
 }
