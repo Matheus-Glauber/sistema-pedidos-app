892f25741066de30832f480d250411f096801544
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-25 22:17:16 -0300

    Adicionando as Exceptions nas Classes de serviços

--- src/main/java/br/com/catrix/services/ProdutoService.java
+++ src/main/java/br/com/catrix/services/ProdutoService.java
@@ -1,12 +1,14 @@
 package br.com.catrix.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.catrix.entities.Produto;
 import br.com.catrix.repositories.ProdutoRepository;
+import br.com.catrix.services.exception.NegocioNotFoundException;
 
 @Service
 public class ProdutoService {
@@ -18,8 +20,9 @@ public class ProdutoService {
 		return produtoRepository.findAll();
 	}
 	
-	public Produto buscarProdutoPorId(Long id) {
-		return produtoRepository.findById(id).get();
+	public Produto buscarProdutoPorId(Long id) throws NegocioNotFoundException {
+		Optional<Produto> produto = produtoRepository.findById(id);
+		return produto.orElseThrow(() -> new NegocioNotFoundException("Produto não encontrado, Id: " + id));
 	}
 
 }
