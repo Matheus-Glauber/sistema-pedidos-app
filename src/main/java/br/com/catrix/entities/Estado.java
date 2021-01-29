5d1ed1c7b20269836d82a13ac8d20a232f24c951
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-01-28 22:37:51 -0300

    Criação das entidades Cidade e Estado

---
+++ src/main/java/br/com/catrix/entities/Estado.java
@@ -0,0 +1,46 @@
+package br.com.catrix.entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "ESTADO")
+@EqualsAndHashCode(exclude = {"estado"}) @RequiredArgsConstructor @NoArgsConstructor @ToString
+public class Estado implements Serializable {
+
+	private static final long serialVersionUID = -3736968647931261525L;
+	
+	@Getter
+	@ApiModelProperty(value = "Código do Estado")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long id;
+	
+	@Getter @Setter
+	@ApiModelProperty(value = "Estado")
+	@Column(name = "ESTADO")
+	private String estado;
+	
+	@Getter @Setter
+	@OneToMany(mappedBy = "estado")
+	private List<Cidade> cidades = new ArrayList<Cidade>();
+
+}
