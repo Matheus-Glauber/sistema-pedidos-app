7a76a8db2f965b6e7755b5a5993846b0f81d2c0b
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-02-01 16:37:35 -0300

    Adicionado o JsonIgnore no atributo cliente da entidade Endereco, para evitar recursividade.

--- src/main/java/br/com/catrix/entities/Endereco.java
+++ src/main/java/br/com/catrix/entities/Endereco.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import io.swagger.annotations.ApiModelProperty;
 
 @Entity
@@ -47,6 +49,7 @@ public class Endereco implements Serializable {
 
 	@ManyToOne
 	@JoinColumn(name = "CLIENTE_ID")
+	@JsonIgnore
 	private Cliente cliente;
 
 	@ManyToOne
