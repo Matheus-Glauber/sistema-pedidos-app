9ea94299f6ca1c5ee62fd0f835fe42ea8eaafe7d
Matheus Glauber Rodrigues Jordão <glauber.jordao1995@outlook.com>
2021-02-05 21:29:30 -0300

    Paginação

--- src/main/java/br/com/catrix/controllers/CategoriaController.java
+++ src/main/java/br/com/catrix/controllers/CategoriaController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -93,6 +95,17 @@ public class CategoriaController {
 	public ResponseEntity<Void> deletarCategoria(@PathVariable Long id) {
 		categoriaService.deletarCategoria(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/page", method = RequestMethod.GET, produces = "application/json")
+	public ResponseEntity<Page<CategoriaDTO>> buscarPaginado(
+			@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {	
+		Page<Categoria> listaCategoria = categoriaService.findPage(page, linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listaDto = listaCategoria.map(cat -> new CategoriaDTO(cat));
+		return ResponseEntity.ok().body(listaDto);
 	}
 
 }
--- src/main/java/br/com/catrix/services/CategoriaService.java
+++ src/main/java/br/com/catrix/services/CategoriaService.java
@@ -5,6 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import br.com.catrix.entities.Categoria;
@@ -13,15 +17,15 @@ import br.com.catrix.services.exception.NegocioNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	public Categoria buscar(Long id) throws NegocioNotFoundException {
 		Optional<Categoria> cat = categoriaRepository.findById(id);
 		return cat.orElseThrow(() -> new NegocioNotFoundException("Categoria não encontrada, Id: " + id));
 	}
-	
+
 	public List<Categoria> buscarTodos() {
 		return categoriaRepository.findAll();
 	}
@@ -34,14 +38,20 @@ public class CategoriaService {
 		buscar(categoria.getId());
 		return categoriaRepository.save(categoria);
 	}
-	
+
 	public void deletarCategoria(Long id) {
 		buscar(id);
 		try {
-			categoriaRepository.deleteById(id);			
+			categoriaRepository.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new NegocioNotFoundException("Não é possível excluir uma categoria que possua produtos relacionados.");
+			throw new NegocioNotFoundException(
+					"Não é possível excluir uma categoria que possua produtos relacionados.");
 		}
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return categoriaRepository.findAll(pageRequest);
 	}
 
 }
